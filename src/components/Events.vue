9b24d654b815d9da53e389126a1bd3c57f5ee47e
Pierre Geffriaud <geffriaud.pierre@gmail.com>
2018-07-05 21:28:18 +0200

    Add pug and route to another component

---
+++ src/components/Events.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+  div
+    h1 Incoming events
+    ul
+      li(v-for='x in values') Event {{x}}
+</template>
+
+<script>
+export default {
+  name: 'Events',
+  data () {
+    return {
+      values: [1, 2, 3, 4],
+      msg: 'Event'
+    }
+  }
+}
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,42 +1,19 @@
-<template>
-  <v-container text-xs-center class="homepage">
-    <v-layout row>
-      <v-flex xs6 offset-xs3>
-        <img src="../assets/logo.png">
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Welcome to your improved eprotec experience !</h1>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex xs4>
-        <h2>Navigate through the app</h2>
-      </v-flex>
-      <v-flex xs4>
-        <h2>Additional links</h2>
-      </v-flex>
-      <v-flex xs4>
-        <h2>Development helps</h2>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex xs4>
-        <v-btn color="primary" href="people"><v-icon left>people</v-icon>Events</v-btn>
-        <v-btn color="primary" href="events"><v-icon left>event</v-icon>People</v-btn>
-      </v-flex>
-      <v-flex xs4>
-        <a href="https://www.facebook.com/ProtectionCivile.LoireAtlantique/" target="_blank">Facebook</a>
-        <a href="https://twitter.com/ProtecCivile" target="_blank">Twitter</a>
-      </v-flex>
-      <v-flex xs4>
-        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
-        <a href="https://vuetifyjs.com/en/getting-started/quick-start" target="_blank">Vuetify</a>
-        <a href="https://material.io/tools/icons/?style=baseline" target="_blank">Icons</a>
-      </v-flex>
-    </v-layout>
-  </v-container>
+<template lang='pug'>
+  div(class='homepage')
+    v-layout(row)
+      v-flex(xs6 offset-xs3)
+        img(src='../assets/logo.png')
+        h1 Welcome to my new Vue project
+        v-btn(color='primary' href='#/events') Events
+          v-icon(right) event
+        v-btn(color='secondary' href='#/people') People
+          v-icon(right) people
+    v-layout(row class='additional-links')
+      v-flex(xs6 offset-xs3)
+        h2 More links
+        ul
+          li(v-for='item in links')
+            a(:href='item.url' target='_blank') {{item.text}}
 </template>
 
 <script>
@@ -44,13 +21,17 @@ export default {
   name: 'Home',
   data () {
     return {
-      msg: 'Welcome to your improved eprotec experience !'
+      links: [
+        {url: 'http://vuejs-templates.github.io/webpack/', text: 'Docs for this template'},
+        {url: 'https://vuetifyjs.com/en/getting-started/quick-start', text: 'Vuetify.js'},
+        {url: 'https://material.io/tools/icons/?style=baseline', text: 'Icons'},
+        {url: 'https://pugjs.org/api/getting-started.html', text: 'Pug.js'}
+      ]
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1,
 h2 {
@@ -66,5 +47,8 @@ li {
 }
 a {
   color: #F98400;
+}
+.additional-links {
+  margin-top: 50px;
 }
 </style>
